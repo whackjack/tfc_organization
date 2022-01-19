aa13dc5e1aef7fb2f9df43669297a503aba3f61f
Joshua Keplinger <whackjack+github@gmail.com>
2022-01-18 22:12:06 -0600

    fix: removed duplicate workspace

--- main.tf
+++ main.tf
@@ -9,13 +9,3 @@ resource "tfe_oauth_client" "github" {
   oauth_token  = "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"
   service_provider = "github"
 }
-
-resource "tfe_workspace" "tfc_organization" {
-  name               = "tfc-organization"
-  organization       = data.tfe_organization.org.name
-  allow_destroy_plan = false
-  vcs_repo {
-    identifier = "whackjack/tfc_organization"
-    oauth_token_id = tfe_oauth_client.github.oauth_token_id
-  }
-}
